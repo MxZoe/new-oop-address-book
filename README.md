dc7f6b266663d5a66dbbe519bfb6acce05aa6070
Zoe Weinstein <mxzweinstein@gmail.com>
2021-12-01 18:32:27 -0700

    got business and personal addresses working

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 To do:
 check if a type of address has already been added
 make the showPersonal and showBusiness are more dynamic
+  
   (currently personal address must be added first and then business address)
--- js/scripts.js
+++ js/scripts.js
@@ -35,7 +35,7 @@ function Contact(firstName, lastName) {
   this.lastName = lastName;
   this.addresses = {};
   this.currentId = -1;  
-}
+};
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
@@ -51,6 +51,12 @@ Contact.prototype.assignId = function() {
   return this.currentId;
 };
 
+Contact.prototype.findAddress = function(id){
+  if (this.addresses[id] != undefined) {
+    return this.addresses[id];
+  }
+  return false;
+};
 //business logic for Addresses
 
 function Address(type, phoneNumber, email, address1, address2, city, state){
@@ -74,40 +80,48 @@ function displayContactDetails(addressBookToDisplay){
   contactList.html(htmlForContactInfo); 
 }
 
-function showPersonal(contactID){
-  const contact = addressBook.findContact(contactID);
-  const addressArray = contact.addresses;
-  
-
-      $(".phone-number").html(addressArray[0].phoneNumber);
-      $(".email").html(addressArray[0].email);
-      $(".address-1").html(addressArray[0].address1);
-      $(".address-2").html(addressArray[0].address2);
-      $(".city").html(addressArray[0].city);
-      $(".state").html(addressArray[0].state);
-    
+function findPersonal(address){
+  let isPersonal = false;
+  Object.values(address).forEach(function(element){
+    if(element === "personal")
+    isPersonal = true;
+  })
+  return isPersonal;
 }
 
-function showBusiness(contactID){
-  const contact = addressBook.findContact(contactID);
-  const addressArray = contact.addresses;
+function showPersonal(address){    
+  $(".phone-number").html(address.phoneNumber);
+  $(".email").html(address.email);
+  $(".address-1").html(address.address1);
+  $(".address-2").html(address.address2);
+  $(".city").html(address.city);
+  $(".state").html(address.state);
+}
 
-      $(".business-phone-number").html(addressArray[1].phoneNumber);
-      $(".business-email").html(addressArray[1].email);
-      $(".business-address-1").html(addressArray[1].address1);
-      $(".business-address-2").html(addressArray[1].address2);
-      $(".business-city").html(addressArray[1].city);
-      $(".business-state").html(addressArray[1].state);
-    
+function showBusiness(address){
+  $(".business-phone-number").html(address.phoneNumber);
+  $(".business-email").html(address.email);
+  $(".business-address-1").html(address.address1);
+  $(".business-address-2").html(address.address2);
+  $(".business-city").html(address.city);
+  $(".business-state").html(address.state);
 }
 function showContact(contactID){
   const contact = addressBook.findContact(contactID);
+  //for loop to check conact.addresses[i].type === personal || business
   $("#show-contact").show();
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
-  showPersonal(contactID);
-  showBusiness(contactID);
-
+  let index = 0;
+  while(contact.addresses[index] != null){
+    let isPersonal = findPersonal(contact.addresses[index]);
+    if(isPersonal){
+      showPersonal(contact.addresses[index]);
+    } else{
+      showBusiness(contact.addresses[index]);
+    }
+    index++;
+  }
   let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
@@ -144,7 +158,6 @@ $(document).ready(function(){
 
   $("form#new-contact").submit(function(event){
     event.preventDefault();
-    //let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedAddress, inputtedAddress2, inputtedCity, selectedState);
     addressBook.addContact(currentContact);
     displayContactDetails(addressBook);
     emptyForm();
@@ -162,17 +175,21 @@ $(document).ready(function(){
     const inputtedCity = $("input#new-city").val();
     const selectedState = $("select#new-state").val();
     let newAddress = new Address(selectedType, inputtedPhoneNumber, inputtedEmail, inputtedAddress, inputtedAddress2, inputtedCity, selectedState);
-
-  
-    
-    /*
-      for each address in current contact, if address.type = selectedType return an alert stating already contains that type
-      else do the thing
-    */
+ /*   let addressArray = currentContact.addresses;
+    if(addressArray.length > 0){
+      addressArray.forEach(function(element){
+        if(element.type === selectedType){
+          alert("You have already added a" + selectedType + "address");
+        } else{
+         
+        }
+      });
+    }
+  */
 
     currentContact.firstName = inputtedFirstName;
     currentContact.lastName = inputtedLastName;
-    
+    currentContact.addAddress(newAddress);
     emptyForm();
   });
 });
